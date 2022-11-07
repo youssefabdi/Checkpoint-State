ff6880a824b25f33a4c6bcedabd7d9a9c51b3588
youssefabdi <youssefelabdi2@gmail.com>
2022-11-07 09:21:42 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+import youssef  from './youssef.jpg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+
+      SHOWWW : false,
+      Person : { 
+                fullName : "Mr. ABDI Youssef",
+                bio : "I hold a Mechatrnic Engineer", 
+                imgSrc: youssef, 
+                profession:"I'm an Engineer"
+              },
+    }
+    this.clikk = this.clikk.bind(this);
+  }
+  
+
+
+  clikk(){
+    this.setState(function (previous){
+      return{
+        SHOWWW : !previous.SHOWWW ,
+      }
+    });
+  }
+
+  render(){
+    if(this.state.SHOWWW)
+    return (
+      <div className='App'>
+        <button onClick={this.clikk}>HIDEE</button>
+        <br/>
+        <img className='img' src={youssef} alt="Personalimg" width="250" />
+        <h1 className='name'>{this.state.Person.fullName}</h1>
+        <h2 className='style'>{this.state.Person.bio}</h2>
+        <h2 className='style'>{this.state.Person.profession}</h2>
+      </div>
+      );
+      else return (
+      
+      <button  onClick={this.clikk}>SHOWWW</button>
+     
+      );
+  }
 }
 
 export default App;
